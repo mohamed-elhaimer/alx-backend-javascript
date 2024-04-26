bf79d61ced0cde3db883b540e34344c9879db173
Logic <mohamedelheimer0@gmail.com>
2024-04-26 15:45:45 +0100

    all

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,21 @@
 import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
-export default function handleProfileSignup (firstName, lastName, fileName) {
-  Promise.all([signUpUser(), uploadPhoto()]).then((value) => {
-    const arr = [
-      obj = {
-        status: Promise.status,
-        value
-      }
-    ];
-    return arr;
-  }).catch((error) => {
-    const arr = [
-       obj = {
-        status: Promise.status,
-        value: error
-      }
-    ];
-    return arr;
-  });
+import { uploadPhoto } from './utils';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      const arr = results.map((result) => {
+        if (result.status === 'fulfilled') {
+          return {
+            status: 'fulfilled',
+            value: result.value,
+          };
+        }
+        return {
+          status: 'rejected',
+          value: result.reason,
+        };
+      });
+      return arr;
+    });
 }

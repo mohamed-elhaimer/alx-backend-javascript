cac4696ee190113512161da8020d9505d7130d2d
Logic <mohamedelheimer0@gmail.com>
2024-04-29 19:31:42 +0100

    all

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+}

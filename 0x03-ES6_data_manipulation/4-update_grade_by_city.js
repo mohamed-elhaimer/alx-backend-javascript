7f2027b6cab876a8be7ffd30ab5556c228249a67
Logic <mohamedelheimer0@gmail.com>
2024-05-01 16:29:12 +0100

    all

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,18 @@
-export default function updateStudentGradeByCity(listStudents, city, newGrades){
-    
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+  const arr = [];
+  if (Array.isArray(listStudents) && typeof city === 'string' && Array.isArray(newGrades)) {
+    const filtrestudents = listStudents.filter(
+      (studentelement) => studentelement.location === city,
+    );
+    // eslint-disable-next-line array-callback-return
+    filtrestudents.map((student) => {
+      const matchegrades = newGrades.find((studentgrade) => student.id === studentgrade.studentId);
+      if (matchegrades) {
+        arr.push({ ...student, grade: matchegrades.grade });
+      } else {
+        arr.push({ ...student, grade: 'N/A' });
+      }
+    });
+  }
+  return arr;
 }

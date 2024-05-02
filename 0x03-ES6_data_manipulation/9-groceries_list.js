aa485fbf20b33726c01c14cbcbe42692890bf895
Logic <mohamedelheimer0@gmail.com>
2024-05-02 02:16:39 +0100

    all

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,8 +1,9 @@
 export default function groceriesList() {
-  return new Map([['Apples', 10],
+   const map = new Map([['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 10],
   ]);
+  return map
 }
